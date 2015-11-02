f9f2cfd75a7e52207eeb2e61a0f44341cdb889ea
Jo Kerkhofs <jke@thinkerit.be>
2015-11-02 16:36:08 +0100

    Added site settings template and messages

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+    minifyHtml = require('gulp-minify-html'),
+    templateCache = require('gulp-angular-templatecache');
+
+var minifyHtmlOpts = {
+    empty: true,
+    cdata: true,
+    conditionals: true,
+    spare: true,
+    quotes: true
+};
+
+gulp.task('bootstrap3', function () {
+    gulp.src('template/bootstrap3/*.html')
+        .pipe(minifyHtml(minifyHtmlOpts))
+        .pipe(templateCache('basic-app-tpls-bootstrap3.js', {standalone: false, module: 'basic.app'}))
+        .pipe(gulp.dest('src'));
+});
+
+gulp.task('default', ['bootstrap3']);
